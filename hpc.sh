ac121c56b6450ae14750e6458078f10c495d7749
John Stitt <jmstitt@gmail.com>
2011-03-23 23:03:52 -0500

    Fixed the hpc.sh file. Broke it when I added comments. (TESTING IS IMPORTANT)

--- hpc.sh
+++ hpc.sh
@@ -11,19 +11,16 @@ cd livecd
 #Clean up system build directories.
 lh clean
 
-#Configure live-helper with options for the iso in the next section
+#Next section (starting with "lh config" handles the configuration of the build.
+     #The line starting "-- packages" tells live-helper which packages we would like installed. It will use apt-get for this, so dependencies are handled.
+     #"--binary indices none" instructs live-helper not to include apt indices. This requires "apt-get update" to be run before other apt commands(the first time), and is used to help keep the iso size small.
+     #The next two lines ("-a" and "-k") instruct to build a 32-bit iso.
+     #"--hostname" sets the hostname to be used on the build.
 lh config \
-    #Tell live-helper which packages we would like installed. It
-    #will use apt-get for this, so dependencies are handled.
     --packages "rsh-server update-cluster openssh-server gcc gfortran sl unison build-essential nfs-kernel-server host john elinks libblas-dev python less gzip rsync" \
-    #Instruct live-helper not to include apt indices. This
-    #Requires "apt-get update" to be run before other apt commands
-    #(the first time), and is used to help keep the iso size small.
     --binary-indices none \
-    #The next two lines instruct to build a 32-bit iso.
     -a 'i386' \
     -k '686' \
-    #Set the hostname to be used on the build by default.
     --hostname aeschylus
 
 #Copy files from chroot-locale-includes into the config directory
